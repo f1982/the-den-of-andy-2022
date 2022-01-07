b582a15ad95eaf26c28cd173f3b6044192f4e917
Andy Cao <caojundan@gmail.com>
2022-01-07 17:31:17 +1300

    feat: update menu bar

---
+++ src/components/Icons/ArrowRight.tsx
@@ -0,0 +1,9 @@
+function ArrowRight() {
+  return (
+    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+      <path d="M5 12h14M12 5l7 7-7 7" />
+    </svg>
+  );
+}
+
+export default ArrowRight;
--- src/components/Layout/header.tsx
+++ src/components/Layout/header.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
+import React from 'react';
 import Logo from '../Logo';
+import ArrowRight from '../Icons/ArrowRight';
 
 interface MenuItemType {
   link:string,
@@ -33,10 +35,10 @@ const MenuData: MenuItemType[] = [
   },
 ];
 
-function MenuItem({ link, label }: MenuItemType):JSX.Element {
+function MenuItem({ link, label }: MenuItemType) {
   return (
     <Link href={link} passHref>
-      <button type="button">{label}</button>
+      <button className="px-3 py-1" type="button">{label}</button>
     </Link>
   );
 }
@@ -48,14 +50,16 @@ export default function Header() {
         <Logo />
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           {
-            MenuData.map((item) => <MenuItem key={item.link} link={item.link} label={item.label} />)
+            MenuData.map((item) => (
+              <span key={item.link} className="mr-2">
+                <MenuItem link={item.link} label={item.label} />
+              </span>
+            ))
           }
         </nav>
         <button type="button" className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
           Button
-          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7" />
-          </svg>
+          <ArrowRight />
         </button>
       </div>
     </header>
