e2242029a7dfd25a831da90143b8e7287ff69043
Andy Cao <caojundan@gmail.com>
2022-01-12 23:33:45 +1300

    fix: update base path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  basePath: '/the-den-of-andy-2022',
+  // basePath: '/the-den-of-andy-2022',
   images: {
     domains: ['dummyimage.com'],
     // https://github.com/vercel/next.js/issues/21079
