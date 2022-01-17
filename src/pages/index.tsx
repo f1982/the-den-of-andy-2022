e7dc6501b8a148becdb3ccb5118bd6ad095593bb
Andy Cao <caojundan@gmail.com>
2022-01-17 15:05:56 +1300

    feat: add cypress e2e testing lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function Index() {
           greeting={t('greeting')}
           link={(
             <Link href="/home" passHref>
-              <button type="button" className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Go</button>
+              <a className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Go</a>
             </Link>
           )}
         />
