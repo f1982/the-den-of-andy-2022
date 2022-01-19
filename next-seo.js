2c8b841a937c6305630165ef540ae8655ac54e1a
Andy Cao <caojundan@gmail.com>
2022-01-19 16:07:03 +1300

    fix: spell check issue

--- next-seo.js
+++ next-seo.js
@@ -1,17 +1,26 @@
+const SITE_NAME = 'Andy website SEO title';
+const SITE_DESC = 'This this andy website seo desc.';
+
 const seoConfig = {
-  title: 'Using More of Config',
-  description: 'This example uses more of the available config options.',
-  canonical: 'https://www.canonical.ie/',
+  title: SITE_NAME,
+  description: SITE_DESC,
+  canonical: process.env.NEXT_PUBLIC_APP_SITE_URL,
   openGraph: {
     type: 'website',
-    locale: 'en_IE',
-    url: 'https://www.url.ie/',
-    site_name: 'SiteName',
+    title: SITE_NAME,
+    description: 'this is the desc will show up in twitter sharing',
+    // if you want to localize your tags, it would probably make sense to include locale.
+    // the format is language_TERRITORY, where the default is en_US.
+    locale: 'en_US',
+    url: process.env.NEXT_PUBLIC_APP_SITE_URL,
+    site_name: SITE_NAME,
+    // video:''
   },
+  // https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/abouts-cards
   twitter: {
-    handle: '@handle',
-    site: '@site',
-    cardType: 'summary_large_image',
+    handle: '@iandycao',
+    creator: '@iandycao',
+    cardType: 'summary',
   },
 };
 
