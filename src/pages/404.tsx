91508ea63d687134b8a1919a8c351b421bf67018
Andy Cao <caojundan@gmail.com>
2022-02-18 11:37:17 +1300

    feat: change layout of 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,15 +5,28 @@ import DotLoader from '../components/Loader/DotLoader';
 import TabTitle from '../components/Metadata/SiteSEO';
 import Headline from '../components/Typography/Headline';
 
-export default function Custom404() {
+function Custom404() {
   return (
     <>
       <TabTitle pageTitle={t('pageNotFound.pageTitle')} />
-      <div className="container mx-auto px-5">
+      <div className="flex-row border-4 justify-center items-center  justify-items-center content-center">
         <Headline title={t('pageNotFound.headline')} />
         <DotLoader />
-        <MyButton href="/" type="primary">{t('pageNotFound.backButtonLabel')}</MyButton>
+        <DotLoader />
+        <div>
+          <MyButton href="/" type="primary">{t('pageNotFound.backButtonLabel')}</MyButton>
+        </div>
       </div>
     </>
   );
 }
+
+Custom404.getLayout = function getLayout(page) {
+  return (
+    <div className="grid place-items-center h-screen">
+      {page}
+    </div>
+  );
+};
+
+export default Custom404;
