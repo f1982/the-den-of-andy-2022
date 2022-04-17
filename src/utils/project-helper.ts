4f2bf0be268255c46a056895bae4cb082270d79f
Andy Cao <caojundan@gmail.com>
2022-04-18 09:45:29 +1200

    feat: add project extra md file

--- src/utils/project-helper.ts
+++ src/utils/project-helper.ts
@@ -1,7 +1,10 @@
 import fs from 'fs';
+import matter from 'gray-matter';
 import { join } from 'path';
+import { ProjectItemData } from '../types/projects';
 
 const PROJECTS_DATA = join(process.cwd(), 'public/static/projects.json');
+const PROJ_POST_DIRECTORY = join(process.cwd(), 'public/static/projects');
 
 export function getProjects() {
   const fullPath = join(PROJECTS_DATA);
@@ -14,3 +17,24 @@ export function getProjectDetail(slug) {
   const { projects } = getProjects();
   return projects.find((item) => item.id === slug);
 }
+
+/**
+ * get a single post info from md file and the folder name
+ *
+ * @param slug
+ * @param fields the data hash of single post
+ * @returns
+ */
+// export function getProjectExtension(slug, lang = 'en', fields = []) {
+//   // slug is the folder name
+//   // could add a zh-cn.md to contain other language content
+//   const fullPath = join(PROJ_POST_DIRECTORY, `${slug}/${lang}.md`);
+//   const fileContents = fs.readFileSync(fullPath, 'utf8');
+//   const { data, content } = matter(fileContents);
+
+//   const projItem:ProjectItemData = {};
+
+//   projItem.content = content;
+
+//   return projItem;
+// }
