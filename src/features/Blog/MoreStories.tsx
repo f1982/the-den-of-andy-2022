f3494560de4f76abe1434d839914b093afe103fd
Andy Cao <caojundan@gmail.com>
2022-06-23 08:47:48 +1200

    style(project&blog): item image size

--- src/features/Blog/MoreStories.tsx
+++ src/features/Blog/MoreStories.tsx
@@ -1,18 +1,15 @@
 import PostPreview from './PostPreview';
 
-// md:gap-x-16
-// md:gap-y-16
-
 export default function MoreStories({ posts }) {
   return (
     <section>
-
       <div className="
       grid grid-cols-1
       md:grid-cols-2
       gap-x-16
       gap-y-16
-      mb-16"
+      mb-16
+      mx-2 md:mx-auto"
       >
         {/* <div className="flex flex-wrap mb-3"> */}
         {posts.map((post) => (
