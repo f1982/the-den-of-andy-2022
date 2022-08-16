1db32853077a972e70319f99061d7bd85e207e35
Andy Cao <caojundan@gmail.com>
2022-08-16 17:44:17 +1200

    feat: add og image and seo description

--- next-seo.js
+++ next-seo.js
@@ -8,7 +8,8 @@ const seoConfig = {
   openGraph: {
     type: 'website',
     title: SITE_NAME,
-    description: 'this is the desc will show up in twitter sharing',
+    // this is the desc will show up in twitter sharing
+    description: 'Andy\'s personal website is used for recording all his work and thinking.',
     // if you want to localize your tags, it would probably make sense to include locale.
     // the format is language_TERRITORY, where the default is en_US.
     locale: 'en_US',
@@ -18,21 +19,19 @@ const seoConfig = {
     // TODO: replace the images
     images: [
       {
-        url: 'https://www.example.ie/og-image-01.jpg',
-        width: 800,
-        height: 600,
-        alt: 'Og Image Alt',
+        url: `${process.env.NEXT_PUBLIC_APP_SITE_URL}/static/image/og-image-1200x627.png`,
+        width: 1200,
+        height: 627,
+        alt: SITE_NAME,
         type: 'image/jpeg',
       },
       {
-        url: 'https://www.example.ie/og-image-02.jpg',
+        url: `${process.env.NEXT_PUBLIC_APP_SITE_URL}/static/image/og-image-900x800.png`,
         width: 900,
         height: 800,
-        alt: 'Og Image Alt Second',
+        alt: SITE_NAME,
         type: 'image/jpeg',
       },
-      { url: 'https://www.example.ie/og-image-03.jpg' },
-      { url: 'https://www.example.ie/og-image-04.jpg' },
     ],
   },
   // https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/abouts-cards
