0493c282b3a522790a541444f718a30388778937
Andy Cao <caojundan@gmail.com>
2022-10-01 21:55:33 +1300

    fix: project detail page seo issue

--- src/pages/project/[slug].tsx
+++ src/pages/project/[slug].tsx
@@ -1,11 +1,11 @@
+import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/router';
 import React from 'react';
-import Modal from 'react-modal';
 import CloseButton from '../../components/atoms/buttons/CloseButton';
-import customModalStyles from '../../constants/modelConfig';
-import ProjectDetail from '../../features/Project/ProjectDetail';
-import { ProjectItemData } from '../../types/projects';
 import { getProjectDetail, getProjects } from '../../features/Project/project-helper';
+import ProjectDetail from '../../features/Project/ProjectDetail';
+import usePageURL from '../../hooks/usePageURL';
+import { ProjectItemData } from '../../types/projects';
 
 interface ProjectPageProps {
   detail: ProjectItemData,
@@ -17,18 +17,34 @@ type ProjectPageType = React.FC<ProjectPageProps> & {
 
 const ProjectPage: ProjectPageType = ({ detail }) => {
   const router = useRouter();
+  const pageURL = usePageURL();
 
   const handleClose = () => {
     router.push('/project');
   };
 
   return (
-    <Modal
-      isOpen
-      onRequestClose={handleClose}
-      style={customModalStyles}
-      contentLabel="Post modal"
-    >
+    <>
+      <NextSeo
+        title={detail.title}
+        additionalMetaTags={[
+          { name: 'keywords', content: detail.tech }, // keywords metadata
+        ]}
+        openGraph={{
+          title: detail.title,
+          description: detail.description,
+          url: pageURL,
+          type: 'project',
+          images: [
+            {
+              url: detail.cover,
+              width: 850,
+              height: 650,
+              alt: `Photo of ${detail.title}`,
+            },
+          ],
+        }}
+      />
       <article className="mb-32">
         <div className="flex container mx-auto mt-8">
           <div className="flex-1" />
@@ -36,7 +52,7 @@ const ProjectPage: ProjectPageType = ({ detail }) => {
         </div>
         <ProjectDetail {...detail} />
       </article>
-    </Modal>
+    </>
   );
 };
 
