5dc2c508f2529fe13892e21ad6bea895f85b7bef
Andy <caojundan@gmail.com>
2024-01-29 08:35:18 +1300

    fix: git act

--- next.config.js
+++ next.config.js
@@ -3,9 +3,16 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: 'export',
   // must have this when using the secondary domain of the github page
   // basePath: '/the-den-of-andy-2022',
   images: {
+    unoptimized: true,
     domains: ['github.com'],
     formats: ['image/avif', 'image/webp'],
     // https://github.com/vercel/next.js/issues/21079
