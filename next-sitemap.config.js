56b020e819aa8130cce63120bc2fb4956268c4ab
Andy <caojundan@gmail.com>
2024-06-15 09:56:19 +1200

    feat: update onekey links

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,9 +2,8 @@
 
 module.exports = {
   siteUrl: 'https://andycao.me',
-  generateRobotsTxt: false, // the robots.text generated with wrong format
+  generateRobotsTxt: true, // the robots.text generated with wrong format
   sitemapSize: 7000, // if it's bigger than 7000, will create sitemap-2.xml
   changefreq: 'weekly',
-  outDir: './out',
-  exclude: ['/manifest.*', '/api', '/tests'],
+  exclude: ['/manifest.*', '/api'],
 }
--- src/features/blog/components/blog-post-card-view.tsx
+++ src/features/blog/components/blog-post-card-view.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Link from 'next/link'
 
+import { parseDate } from '@/utils/utils'
+
 import HoverScaleImage from '@/components/atoms/hover-scale-image'
 
 interface PostPreviewProps {
@@ -22,7 +24,7 @@ const BlogPostCardView: React.FC<PostPreviewProps> = ({
 
     <div className="mt-3">
       <h3 className="line-clamp-1 font-semibold">{title}</h3>
-      <span className="text-xs text-muted-foreground">{date}</span>
+      <span className="text-xs text-muted-foreground">{parseDate(date)}</span>
     </div>
   </Link>
 )
